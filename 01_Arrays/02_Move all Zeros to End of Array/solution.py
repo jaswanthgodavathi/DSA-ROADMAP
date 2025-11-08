c4ef3424b3126c297dc9fcff51b6c2c56496023c
jaswanthgodavarthi <jaswanthgodavarthy@gmail.com>
2025-11-08 17:28:48 +0530

    hiteesh loves coding

--- 01_Arrays/02_Move all Zeros to End of Array/solution.py
+++ 01_Arrays/02_Move all Zeros to End of Array/solution.py
@@ -1,1 +1,10 @@
+from typing import List
 
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[count] = nums[count], nums[i]
+                count += 1
+        return nums
