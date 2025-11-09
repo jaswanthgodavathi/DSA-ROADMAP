bcb25ba84e6849b3918f9370b230a02728609012
hiteesh333 <129778341+hiteesh333@users.noreply.github.com>
2025-11-09 11:29:45 +0530

    Update solution.cpp

--- 01_Arrays/03_Remove duplicates from Sorted Array/solution.cpp
+++ 01_Arrays/03_Remove duplicates from Sorted Array/solution.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int>s;
+        for(auto i:nums){
+            if(s.count(i)) return true;
+            s.insert(i);
+        }
+        return false;
+    }
+};
