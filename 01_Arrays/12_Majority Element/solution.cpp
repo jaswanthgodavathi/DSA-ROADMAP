0caff9529b2d39e4a8a6b226511e7ab5d158210e
hiteesh333 <129778341+hiteesh333@users.noreply.github.com>
2025-11-09 19:18:41 +0530

    Update solution.cpp

--- 01_Arrays/12_Majority Element/solution.cpp
+++ 01_Arrays/12_Majority Element/solution.cpp
@@ -1,1 +1,13 @@
-
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        map<int,int>mpp;
+        for(auto i:nums) mpp[i]++;
+        for(auto it:mpp){
+            if(it.second>ceil(n/2)) return it.first;
+        }
+        return -1;
+    }
+};
