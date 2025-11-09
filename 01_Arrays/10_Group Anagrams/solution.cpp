cd6f59cc983fd27ac79841b987353b2d17769f98
hiteesh333 <129778341+hiteesh333@users.noreply.github.com>
2025-11-09 19:51:23 +0530

    Update solution.cpp

--- 01_Arrays/10_Group Anagrams/solution.cpp
+++ 01_Arrays/10_Group Anagrams/solution.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mpp;
+        for(auto i:strs){
+            string key=i;
+            sort(key.begin(),key.end());
+            mpp[key].push_back(i);
+        }
+        vector<vector<string>>ans;
+        for(auto it:mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
 
