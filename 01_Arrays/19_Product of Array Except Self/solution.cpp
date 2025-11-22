515038a818546ff9e2369d3013e87584273e69ea
hiteesh333 <129778341+hiteesh333@users.noreply.github.com>
2025-11-22 09:43:43 +0530

    Update solution.cpp

--- 01_Arrays/19_Product of Array Except Self/solution.cpp
+++ 01_Arrays/19_Product of Array Except Self/solution.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        int pref=1;
+        int suf=1;
+        vector<int>prod(n,1);
+        for(int i=0;i<n;i++){
+            prod[i]=pref;
+            pref *=nums[i];
+        }
+        for(int i=n-1;i>=0;i--){
+            prod[i] *=suf;
+            suf *=nums[i];
+        }
+        return prod;
+    }
+};
 
