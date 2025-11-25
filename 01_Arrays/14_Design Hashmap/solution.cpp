acd66feb9e924b8ca2751c4db14a9d3d83212426
hiteesh333 <129778341+hiteesh333@users.noreply.github.com>
2025-11-25 10:34:28 +0530

    Update solution.cpp

--- 01_Arrays/14_Design Hashmap/solution.cpp
+++ 01_Arrays/14_Design Hashmap/solution.cpp
@@ -1,1 +1,19 @@
-
+class MyHashMap {
+public:
+    MyHashMap() {
+        
+    }
+    unordered_map<int,int>m;
+    void put(int key, int value) {
+        m[key]=value;
+    }
+    
+    int get(int key) {
+        if(m.find(key) !=m.end()) return m[key];
+        else return -1;
+    }
+    
+    void remove(int key) {
+        m.erase(key);
+    }
+};
